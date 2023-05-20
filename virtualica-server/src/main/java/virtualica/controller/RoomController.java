5b995c872161f70e3413c2006d30d34b4df5b6b2
Aprijal Ghiyas Setiawan <aprijalghiyas@gmail.com>
2023-05-20 10:26:51 +0700

    Add RoomController class

---
+++ virtualica-server/src/main/java/virtualica/controller/RoomController.java
@@ -0,0 +1,11 @@
+package virtualica.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(path = "/room")
+public class RoomController {
+}
--- virtualica-server/src/main/java/virtualica/controller/UserController.java
+++ virtualica-server/src/main/java/virtualica/controller/UserController.java
@@ -1,18 +1,27 @@
 package virtualica.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
+import virtualica.dto.UserDto;
+import virtualica.service.UserService;
+import virtualica.util.ApiResponse;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping(path = "/user")
 public class UserController {
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getUser() {
-        return ResponseEntity.ok(Map.of("email", "aprijalghiyas@gmail.com"));
+    private final UserService userService;
+
+    @PostMapping(path = "/signup", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> userSignup(@RequestBody UserDto userDto) {
+        userService.saveUser(userService.dtoToEntity(userDto));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.jsonStructureNoData(HttpStatus.CREATED));
     }
 }
