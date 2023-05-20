1803ea2ee40816ff11aaaf50248ad855a9f3f2c2
Aprijal Ghiyas Setiawan <aprijalghiyas@gmail.com>
2023-05-21 06:30:17 +0700

    Modify _document.tsx file

--- virtualica-client/pages/_document.tsx
+++ virtualica-client/pages/_document.tsx
@@ -1,13 +1,64 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentProps,
+  DocumentContext
+} from 'next/document';
+import {MyAppProps} from './_app';
+import {AppType} from 'next/app';
+import createEmotionServer from '@emotion/server/create-instance';
+import createEmotionCache from '../functions/createEmotionCache';
+import theme from '../functions/theme';
 
-export default function Document() {
+interface MyDocumentProps extends DocumentProps {
+  emotionStyleTags: JSX.Element[],
+}
+
+export default function MyDocument({ emotionStyleTags }: MyDocumentProps) {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <meta name='theme-color' content={theme.palette.primary.main} />
+        <link rel='shortcut icon' href='/favicon.ico' />
+        <meta name='emotion-insertion-point' content='' />
+        {emotionStyleTags}
+      </Head>
       <body>
-        <Main />
-        <NextScript />
+      <Main />
+      <NextScript />
       </body>
     </Html>
-  )
+  );
+}
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const originalRenderPage = ctx.renderPage;
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App: React.ComponentType<React.ComponentProps<AppType> & MyAppProps>) =>
+        function EnhanceApp(props) {
+          return <App emotionCache={cache} {...props} />;
+        },
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+
+  return {
+    ...initialProps,
+    emotionStyleTags,
+  }
 }
--- virtualica-client/pages/room/index.tsx
+++ virtualica-client/pages/room/index.tsx
@@ -1,3 +1,5 @@
+import {Button} from '@mui/material';
+
 const conn: WebSocket = new WebSocket('ws://localhost:7181/socket');
 
 const Room = () => {
@@ -12,7 +14,7 @@ const Room = () => {
   return (
     <>
       <div>Room</div>
-      <button type='button' onClick={handleSendButton}>Send signal</button>
+      <Button variant='contained'>Send Signal</Button>
     </>
   );
 }
