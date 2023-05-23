ce8849ea50e07857383634b2f413acbda238a3bc
Aprijal Ghiyas Setiawan <aprijalghiyas@gmail.com>
2023-05-23 19:32:57 +0700

    Add login authentication function

--- virtualica-client/components/home_components/LoginDialog.tsx
+++ virtualica-client/components/home_components/LoginDialog.tsx
@@ -11,32 +11,69 @@ import {
   useMediaQuery
 } from '@mui/material';
 import {Close, LockOutlined} from '@mui/icons-material';
-import theme from '@/functions/theme';
+import {useState} from 'react';
+
+type ApiType<T> = {
+  code: number,
+  data?: T,
+  status: string,
+}
+
+type LoginResult = {
+  access_token: string,
+  refresh_token: string,
+}
 
 const LoginDialog = ({ openLoginDialog, handleCloseLoginDialog }: { openLoginDialog: boolean, handleCloseLoginDialog: () => void, }) => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleLogin = async () => {
+    if (email !== '' && password !== '') {
+      const formBody: { [n: string]: string } = { email: email, password: password, }
+      const encodedFormBody = Object.keys(formBody).map((key: string) => encodeURIComponent(key) + '=' + encodeURIComponent(formBody[key])).join('&');
+
+      const fetchResult = await fetch('http://localhost:7181/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8', },
+        body: encodedFormBody,
+      });
+
+      const apiResult: ApiType<LoginResult> = await fetchResult.json();
+      if (apiResult.code === 200) {
+        window.location.href = ('http://localhost:7181/room');
+      }
+    }
+  }
+
   return (
     <Dialog open={openLoginDialog} onClose={handleCloseLoginDialog} maxWidth='xs' fullScreen={useMediaQuery('(max-width: 600px)')}>
       <Box sx={{ maxWidth: '400px', mx: 'auto' }}>
         <DialogTitle sx={{ p: '24px 24px 0', }}>
-          <Avatar sx={{ mx: 'auto', mb: 1, bgcolor: '#199bf1', }}>
+          <Avatar sx={{ mx: 'auto', mb: 1.5, bgcolor: '#199bf1', }}>
             <LockOutlined sx={{ color: 'white', }} />
           </Avatar>
           <Typography gutterBottom sx={{ textAlign: 'center', fontWeight: '600', fontSize: '24px', }}>Login</Typography>
           <Typography align='center'>Welcome, hope you have an amazing day, enjoy your online meetings with Virtualica</Typography>
         </DialogTitle>
         <DialogContent sx={{ flexGrow: '0', p: '24px !important', }}>
-          <TextField fullWidth label='Email Address' variant='outlined' sx={{ mb: 2, }} />
-          <TextField fullWidth label='Password' variant='outlined' sx={{ mb: 1, }} />
+          <TextField
+            fullWidth label='Email Address' variant='outlined'
+            value={email} onChange={(e) => setEmail(e.target.value)} sx={{ mb: 2, }} />
+          <TextField
+            fullWidth label='Password' variant='outlined' type='password'
+            value={password} onChange={(e) => setPassword(e.target.value)} sx={{ mb: 1, }} />
           <FormControlLabel control={<Checkbox disableRipple />} label='Remember me' sx={{ mb: 1, }} />
-          <Button fullWidth variant='contained'>Login</Button>
+          <Button fullWidth variant='contained' onClick={handleLogin}>Login</Button>
 
-          <Typography align='center' sx={{ mt: 1}}>
+          <Typography display='flex' justifyContent='space-between' sx={{ mt: 1}}>
             <Link sx={{ fontSize: '14px', cursor: 'pointer', }}>Forgot your password?</Link>
+            <Link sx={{ fontSize: '14px', cursor: 'pointer', }}>or Sign up</Link>
           </Typography>
         </DialogContent>
       </Box>
       <IconButton onClick={handleCloseLoginDialog} sx={{ position: 'absolute', right: '10px', top: '10px', }}>
-        <Close />
+        <Close fontSize='small' />
       </IconButton>
     </Dialog>
   );
