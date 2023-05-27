50f8ca579d23a42cf46d9556c38ad3676d368e68
Aprijal Ghiyas Setiawan <aprijalghiyas@gmail.com>
2023-05-27 11:57:44 +0700

    Add desktop videos grid

--- virtualica-client/components/room_components/VideoContainer.tsx
+++ virtualica-client/components/room_components/VideoContainer.tsx
@@ -2,7 +2,7 @@ import {Avatar, Box, Typography} from '@mui/material';
 import {grey} from '@mui/material/colors';
 import {MicOffOutlined} from '@mui/icons-material';
 import nameToColor from '@/functions/nameToColor';
-import {createRef, useEffect, useRef, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 
 const Video = ({ name }: { name: string, }) => {
   return (
@@ -46,54 +46,74 @@ const VideoContainer = () => {
   })
 
   const renderVideos = (videos: string[]) => {
-    if ((window.innerHeight > window.innerWidth) && videosWrapperRef.current) {
-      const cellMinWidth = 150;
-      const cellMinHeight = 170;
-      const wrapperWidth: number | undefined = videosWrapperRef.current?.offsetWidth;
-      const wrapperHeight: number | undefined = videosWrapperRef.current?.offsetHeight;
-      const maxRowNum = Math.floor(wrapperHeight / cellMinHeight);
-      const maxColNum = Math.floor(wrapperWidth / cellMinWidth);
-      const maxVideoNum = maxRowNum * maxColNum;
+    let cellMinWidth = 160;
+    let cellMinHeight = 180;
 
-      const renderedVideos = videos.slice(0, maxVideoNum);
-      const gridMap = [];
+    if (window.innerHeight < window.innerWidth) {
+      cellMinWidth = 260;
+      cellMinHeight = 160;
+    }
 
-      let col = 0;
+    const wrapperWidth: number | undefined = videosWrapperRef.current?.offsetWidth;
+    const wrapperHeight: number | undefined = videosWrapperRef.current?.offsetHeight;
+    const maxRowNum = Math.floor(wrapperHeight / cellMinHeight);
+    const maxColNum = Math.floor(wrapperWidth / cellMinWidth);
+    const maxVideoNum = maxRowNum * maxColNum;
+
+    const renderedVideos = videos.slice(0, maxVideoNum);
+    const gridMap = [];
+
+    if (window.innerHeight > window.innerWidth) {
+      let y = 0;
       while (true) {
         if (gridMap.length === renderedVideos.length) break;
-        for (let row = 0; row <= col; row++) {
+        for (let x = 0; x <= y; x++) {
           if (gridMap.length === renderedVideos.length) break;
-          if (row < maxColNum) gridMap.push([row, col]);
+          if ((x < maxColNum) && (y < maxRowNum)) gridMap.push([x, y]);
           if (gridMap.length === renderedVideos.length) break;
-          if (row === col) continue;
-          if (col < maxColNum) gridMap.push([col, row]);
+          if (x === y) continue;
+          if (y < maxColNum) gridMap.push([y, x]);
           if (gridMap.length === renderedVideos.length) break;
         }
-        col++;
+        y++;
       }
-
-      let rowNum = 1;
-      gridMap.map(val => rowNum = (val[1] + 1) > rowNum ? val[1] + 1 : rowNum);
-
-      const groupVideos: string[][] = [];
-      for (let i = 0; i < rowNum; i++) groupVideos.push([]);
-      for (let j = 0; j < renderedVideos.length; j++) groupVideos[gridMap[j][1]].push(renderedVideos[j]);
-
-      return (
-        <Box sx={{ height: '100%', display: 'grid', gridTemplateRows: `repeat(${rowNum}, 1fr)`, rowGap: '10px', }}>
-          {groupVideos.map((videos, idx) => (
-            <Box key={idx} display='flex' columnGap='10px'>
-              {videos.map((val, idx) => <Video key={idx} name={val} />)}
-            </Box>
-          ))}
-        </Box>
-      );
+    } else {
+      let x = 0;
+      while (true) {
+        if (gridMap.length === renderedVideos.length) break;
+        for (let y = 0; y <= x; y++) {
+          if (gridMap.length === renderedVideos.length) break;
+          if ((y < maxRowNum) && (x < maxColNum)) gridMap.push([x, y]);
+          if (gridMap.length === renderedVideos.length) break;
+          if (y === x) continue;
+          if (x < maxRowNum) gridMap.push([y, x]);
+          if (gridMap.length === renderedVideos.length) break;
+        }
+        x++;
+      }
     }
+
+    let rowNum = 1;
+    gridMap.map(val => rowNum = (val[1] + 1) > rowNum ? val[1] + 1 : rowNum);
+
+    const groupVideos: string[][] = [];
+    for (let i = 0; i < rowNum; i++) groupVideos.push([]);
+    for (let j = 0; j < renderedVideos.length; j++) groupVideos[gridMap[j][1]].push(renderedVideos[j]);
+
+    return (
+      <Box sx={{ height: '100%', display: 'grid', gridTemplateRows: `repeat(${rowNum}, 1fr)`, rowGap: '10px', }}>
+        {groupVideos.map((videos, idx) => (
+          <Box key={idx} display='flex' columnGap='10px'>
+            {videos.map((val, idx) => <Video key={idx} name={val} />)}
+          </Box>
+        ))}
+      </Box>
+    );
   }
 
   return (
-    <Box display='flex' flexDirection='column' flexGrow='1' sx={{ mt: 2, }}>
-      <Typography onClick={addVideo} sx={{ mb: 2, '@media (min-width: 600px)': { display: 'none', }, }}>Team Meeting</Typography>
+    <Box display='flex' flexDirection='column' flexGrow='1' sx={{ mt: 2, }} onClick={addVideo}>
+      <Typography sx={{ mb: 2, '@media (min-width: 600px)': { display: 'none', }, }}>Team Meeting</Typography>
       <Box ref={videosWrapperRef} display='flex' flexGrow='1' flexDirection='column' sx={{ overflow: 'hidden', }}>
         {videos && renderVideos(videos)}
       </Box>
