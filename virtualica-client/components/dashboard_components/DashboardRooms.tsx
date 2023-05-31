db05a988f5ab0259b5208ac8bb853a93da57117d
Aprijal Ghiyas Setiawan <aprijalghiyas@gmail.com>
2023-05-31 10:10:52 +0700

    Add room provider

--- virtualica-client/components/dashboard_components/DashboardRooms.tsx
+++ virtualica-client/components/dashboard_components/DashboardRooms.tsx
@@ -18,10 +18,12 @@ import {
   VideoCallOutlined,
   VideocamOutlined
 } from '@mui/icons-material';
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import CreateRoomDialog from '@/components/dashboard_components/CreateRoomDialog';
+import {RoomContext} from '@/context/RoomProvider';
 
 const DashboardRooms = () => {
+  const { roomList, selectedRoom } = useContext(RoomContext);
   const [openCreateRoomDialog, setOpenCreateRoomDialog] = useState<boolean>(false);
 
   return (
@@ -55,20 +57,21 @@ const DashboardRooms = () => {
             ':hover': { '::-webkit-scrollbar-thumb': { visibility: 'visible', }, scrollbarWidth: 'thin', },
           }}
         >
-          {/*<Box display='flex' alignItems='center' columnGap='10px' justifyContent='center' sx={{ height: '100%', }}>*/}
-          {/*  <VideocamOutlined sx={{ color: grey.A400, }} />*/}
-          {/*  <Typography sx={{ fontSize: '14px', color: grey.A400, }}>You haven't created a room</Typography>*/}
-          {/*</Box>*/}
-
-          <List>
-            <ListItemButton>
-              <ListItemText
-                primary={<Typography sx={{ fontWeight: '600', }}>Biochemistry Group Discussion</Typography>}
-                secondary='Created at June 15, 2023'
-              />
-            </ListItemButton>
-            <Divider  component="li" />
-          </List>
+          {roomList.length === 0 ?
+            <Box display='flex' alignItems='center' columnGap='10px' justifyContent='center' sx={{ height: '100%', }}>
+              <VideocamOutlined sx={{ color: grey.A400, }} />
+              <Typography sx={{ fontSize: '14px', color: grey.A400, }}>You haven't created a room</Typography>
+            </Box> :
+            <List>
+              <ListItemButton>
+                <ListItemText
+                  primary={<Typography sx={{ fontWeight: '600', }}>Biochemistry Group Discussion</Typography>}
+                  secondary='Created at June 15, 2023'
+                />
+              </ListItemButton>
+              <Divider  component="li" />
+            </List>
+          }
         </Box>
       </Box>
 
@@ -81,75 +84,76 @@ const DashboardRooms = () => {
           ':hover': { '::-webkit-scrollbar-thumb': { visibility: 'visible', }, scrollbarWidth: 'thin', },
         }}
       >
-        {/*<Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' rowGap='20px' sx={{ height: '100%', }}>*/}
-        {/*  <img src='/room-detail.png' alt='room detail' style={{ maxWidth: '200px', }} />*/}
-        {/*  <Typography sx={{ fontSize: '14px', color: grey.A400, }}>Click the room to show the detail</Typography>*/}
-        {/*</Box>*/}
+        {selectedRoom === '' ?
+          <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' rowGap='20px' sx={{ height: '100%', }}>
+            <img src='/room-detail.png' alt='room detail' style={{ maxWidth: '200px', }} />
+            <Typography sx={{ fontSize: '14px', color: grey.A400, }}>Click the room to show the detail</Typography>
+          </Box> :
+          <Box display='flex' flexDirection='column' sx={{ height: '100%', pr: 1, }}>
+            <Typography gutterBottom variant='h5' sx={{ fontWeight: '600', mb: 2, }}>Biochemistry Group Discussion</Typography>
+            <Box display='flex' columnGap='10px' alignItems='center' sx={{ color: grey['400'], mb: 1, }}>
+              <InfoOutlined fontSize='small' sx={{ mb: '2px', }} />
+              <Typography>Room for discussion of Biochemistry Group 5</Typography>
+            </Box>
+            <Box display='flex' columnGap='10px' alignItems='center' sx={{ color: grey['400'], mb: 1, }}>
+              <Link fontSize='small' sx={{ mb: '2px', transform: 'rotate(-45deg)', }} />
+              <Typography>123e4567-e89b-12d3-a456-426614174000</Typography>
+            </Box>
+            <Box display='flex' columnGap='10px' alignItems='center' sx={{ color: grey['400'], mb: 1, }}>
+              <AccessTime fontSize='small' sx={{ mb: '2px', }} />
+              <Typography>Created at June 15, 2023</Typography>
+            </Box>
+            <Box display='flex' columnGap='10px' alignItems='center' sx={{ color: grey['400'], mb: 1, }}>
+              <PeopleOutlined fontSize='small' sx={{ mb: '2px', }} />
+              <Typography>0 Contacts invited</Typography>
+            </Box>
 
-        <Box display='flex' flexDirection='column' sx={{ height: '100%', pr: 1, }}>
-          <Typography gutterBottom variant='h5' sx={{ fontWeight: '600', mb: 2, }}>Biochemistry Group Discussion</Typography>
-          <Box display='flex' columnGap='10px' alignItems='center' sx={{ color: grey['400'], mb: 1, }}>
-            <InfoOutlined fontSize='small' sx={{ mb: '2px', }} />
-            <Typography>Room for discussion of Biochemistry Group 5</Typography>
-          </Box>
-          <Box display='flex' columnGap='10px' alignItems='center' sx={{ color: grey['400'], mb: 1, }}>
-            <Link fontSize='small' sx={{ mb: '2px', transform: 'rotate(-45deg)', }} />
-            <Typography>123e4567-e89b-12d3-a456-426614174000</Typography>
-          </Box>
-          <Box display='flex' columnGap='10px' alignItems='center' sx={{ color: grey['400'], mb: 1, }}>
-            <AccessTime fontSize='small' sx={{ mb: '2px', }} />
-            <Typography>Created at June 15, 2023</Typography>
-          </Box>
-          <Box display='flex' columnGap='10px' alignItems='center' sx={{ color: grey['400'], mb: 1, }}>
-            <PeopleOutlined fontSize='small' sx={{ mb: '2px', }} />
-            <Typography>0 Contacts invited</Typography>
-          </Box>
+            <Box display='flex' columnGap='10px' rowGap='10px' flexWrap='wrap' sx={{ my: 5, }}>
+              <Button variant='contained' size='small' sx={{ textTransform: 'none', }}>Start</Button>
+              <Button variant='outlined' size='small' startIcon={<ContentCopy />} sx={{ textTransform: 'none', }}>Copy Invitation</Button>
+              <Button variant='outlined' size='small' startIcon={<EditOutlined />} sx={{ textTransform: 'none', }}>Edit</Button>
+              <Button variant='outlined' size='small' startIcon={<DeleteOutlined />} color='error' sx={{ textTransform: 'none', }}>Delete</Button>
+            </Box>
 
-          <Box display='flex' columnGap='10px' rowGap='10px' flexWrap='wrap' sx={{ my: 5, }}>
-            <Button variant='contained' size='small' sx={{ textTransform: 'none', }}>Start</Button>
-            <Button variant='outlined' size='small' startIcon={<ContentCopy />} sx={{ textTransform: 'none', }}>Copy Invitation</Button>
-            <Button variant='outlined' size='small' startIcon={<EditOutlined />} sx={{ textTransform: 'none', }}>Edit</Button>
-            <Button variant='outlined' size='small' startIcon={<DeleteOutlined />} color='error' sx={{ textTransform: 'none', }}>Delete</Button>
+            <Box flexGrow='1'>
+              <Accordion disableGutters sx={{ backgroundImage: 'none', boxShadow: 'none', }}>
+                <AccordionSummary expandIcon={<ExpandMore />} sx={{ minHeight: '48px', px: 0, }}>
+                  <Typography sx={{ fontWeight: '500', }}>Send Invitation to Contacts</Typography>
+                </AccordionSummary>
+                <AccordionDetails sx={{ p: 0, }}>
+                  <List
+                    sx={{
+                      maxHeight: '280px', overflowX: 'hidden', overflowY: 'overlay', scrollbarWidth: 'none',
+                      '::-webkit-scrollbar': { width: '5px', },
+                      '::-webkit-scrollbar-thumb': { borderRadius: '5px', visibility: 'hidden', backgroundColor: 'rgba(255, 255, 255, 0.3)', },
+                      ':hover': { '::-webkit-scrollbar-thumb': { visibility: 'visible', }, scrollbarWidth: 'thin', },
+                    }}
+                  >
+                    {['Deki Geraldi', 'Ainun Nisa', 'Irsyad Ibadurrahman', 'Deki Geraldi', 'Ainun Nisa', 'Irsyad Ibadurrahman', 'Deki Geraldi', 'Ainun Nisa', 'Irsyad Ibadurrahman'].map((val, idx) => (
+                      <ListItem
+                        key={idx}
+                        disablePadding
+                        secondaryAction={
+                          <Checkbox edge='end' />
+                        }
+                      >
+                        <ListItemButton>
+                          <ListItemAvatar>
+                            <Avatar src='https://i.pravatar.cc/150' alt='Contact profile picture' />
+                          </ListItemAvatar>
+                          <ListItemText primary={val} />
+                        </ListItemButton>
+                      </ListItem>
+                    ))}
+                  </List>
+                  <Box display='flex' justifyContent='flex-end' sx={{ mt: 3, }}>
+                    <Button variant='contained' size='small' startIcon={<SendOutlined />} sx={{ textTransform: 'none', }}>Send</Button>
+                  </Box>
+                </AccordionDetails>
+              </Accordion>
+            </Box>
           </Box>
-
-          <Box flexGrow='1'>
-            <Accordion disableGutters sx={{ backgroundImage: 'none', boxShadow: 'none', }}>
-              <AccordionSummary expandIcon={<ExpandMore />} sx={{ minHeight: '48px', px: 0, }}>
-                <Typography sx={{ fontWeight: '500', }}>Send Invitation to Contacts</Typography>
-              </AccordionSummary>
-              <AccordionDetails sx={{ p: 0, }}>
-                <List
-                  sx={{
-                    maxHeight: '280px', overflowX: 'hidden', overflowY: 'overlay', scrollbarWidth: 'none',
-                    '::-webkit-scrollbar': { width: '5px', },
-                    '::-webkit-scrollbar-thumb': { borderRadius: '5px', visibility: 'hidden', backgroundColor: 'rgba(255, 255, 255, 0.3)', },
-                    ':hover': { '::-webkit-scrollbar-thumb': { visibility: 'visible', }, scrollbarWidth: 'thin', },
-                  }}
-                >
-                  {['Deki Geraldi', 'Ainun Nisa', 'Irsyad Ibadurrahman', 'Deki Geraldi', 'Ainun Nisa', 'Irsyad Ibadurrahman', 'Deki Geraldi', 'Ainun Nisa', 'Irsyad Ibadurrahman'].map((val, idx) => (
-                    <ListItem
-                      key={idx}
-                      disablePadding
-                      secondaryAction={
-                        <Checkbox edge='end' />
-                      }
-                    >
-                      <ListItemButton>
-                        <ListItemAvatar>
-                          <Avatar src='https://i.pravatar.cc/150' alt='Contact profile picture' />
-                        </ListItemAvatar>
-                        <ListItemText primary={val} />
-                      </ListItemButton>
-                    </ListItem>
-                  ))}
-                </List>
-                <Box display='flex' justifyContent='flex-end' sx={{ mt: 3, }}>
-                  <Button variant='contained' size='small' startIcon={<SendOutlined />} sx={{ textTransform: 'none', }}>Send</Button>
-                </Box>
-              </AccordionDetails>
-            </Accordion>
-          </Box>
-        </Box>
+        }
       </Box>
 
       {/* CREATE ROOM DIALOG */}
--- virtualica-client/context/RoomProvider.tsx
+++ virtualica-client/context/RoomProvider.tsx
@@ -10,6 +10,7 @@ export type RoomType = {
 
 interface RoomContextProps {
   roomList: RoomType[],
+  selectedRoom: string,
   handleGetRoomList?: () => RoomType[],
   handleGetRoomDetail?: (roomId: string) => UserType,
   handleAddRoomList?: (roomList: RoomType[]) => void,
@@ -19,15 +20,17 @@ interface RoomContextProps {
 
 const initialRoomContext: RoomContextProps = {
   roomList: [],
+  selectedRoom: '',
 }
 
 export const RoomContext = createContext<RoomContextProps>(initialRoomContext);
 
 const RoomProvider = ({ children }: { children: ReactNode, }) => {
   const [roomList, setRoomList] = useState<RoomType[]>(initialRoomContext.roomList);
+  const [selectedRoom, setSelectedRoom] = useState<string>(initialRoomContext.selectedRoom);
 
   return (
-    <RoomContext.Provider value={{ roomList }}>
+    <RoomContext.Provider value={{ roomList, selectedRoom }}>
       {children}
     </RoomContext.Provider>
   );
